48f79a49cb55b6c8f50eda6d6e8df44704c544f5
Jose1i1o <guadix88@gmail.com>
2022-01-06 22:30:52 +0100

    feat(404): 404 page with React Router 6

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import logo from './logo.svg';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
 import ArticleListPage from './pages/ArticleListPage';
+import NotFoundPage from './pages/NotFoundPage';
 import NavBar from './NavBar';
 import './App.css';
 
@@ -17,10 +18,12 @@ class App extends Component {
           <NavBar />
           <div id="page-body">
             <Routes>
-                <Route path="/" element={<HomePage />} exact />
+                <Route path="/" element={<HomePage />} exact/>
                 <Route path="/about" element={<AboutPage />} />
                 <Route path="/articles-list" element={<ArticleListPage />} />
                 <Route path="/article/:name" element={<ArticlePage />} />
+                <Route path="/404" element={<NotFoundPage />} />
+                <Route path="*" element={<Navigate replace to="/404" />} />
             </Routes>
           </div>
         </div>
