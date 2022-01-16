59e546333f9f35f8a1f72a566325788457c09283
Jose1i1o <guadix88@gmail.com>
2022-01-16 13:47:49 +0100

    add useState and useEffect to article upvotes

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articleFeed from './articleFeed';
 import { useParams } from 'react-router-dom';
 import ArticleList from '../components/ArticleList';
 import NotFoundPage from './NotFoundPage';
 
-
 const ArticlePage = ({ match }) => {
+
     const name = useParams(match).name;
     const article = articleFeed.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: 3 });
+    })
 
     if(!article) return <NotFoundPage />;
 
@@ -16,6 +22,7 @@ const ArticlePage = ({ match }) => {
     return(
         <>
             <h1>{ article.title }</h1>
+            <p> This post has been updated { articleInfo.upvotes } times.</p>
             { article.content.map((paragraph, key) => (
                 <p key={key}>{ paragraph }</p>
             ))}
